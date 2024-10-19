6bf4bfcdbdda25a45af0a46b905041544f68a76d
Sagar Chhabriya <129516589+SagarChhabriya@users.noreply.github.com>
2024-10-19 09:40:08 +0000

    update

---
+++ ConsoleApp2_Quiz/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int[] arr = { 20, 30, 10, 50, 40 };
+            Console.WriteLine("Unsorted Array");
+            display(arr);
+            Console.WriteLine("Sorted Array");
+            display(selectionsort(arr));
+            Console.ReadKey();
+
+
+        }
+
+        public static int[] selectionsort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int begin = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[begin])
+                    {
+                        begin = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[begin];
+                arr[begin] = temp;
+            }
+            return arr;
+        }
+
+        public static void display(int[] arr)
+        {
+            for (int lcv = 0; lcv < arr.Length; lcv++)
+            {
+                Console.WriteLine(arr[lcv]);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ DataGridView/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable table_person = new DataTable();
+
+            //table.Columns.Add(
+            //    new DataColumn( CompanyName, typeof(string ) ) );
+
+            table_person.Columns.Add("Rno");
+            table_person.Columns.Add("Name");
+            table_person.Columns.Add("Address");
+
+            table_person.Rows.Add(101, "Sagar", "KK");
+            table_person.Rows.Add(101, "Qadeer", "KK");
+            table_person.Rows.Add(101, "Deepak", "KK");
+
+            DataTable  table_color = new DataTable();
+            
+            table_color.Columns.Add("Color");
+            table_color.Columns.Add("des");
+            table_color.Rows.Add("Red", "good");
+            table_color.Rows.Add("Green", "Exe");
+
+
+
+
+
+
+            // dataGridView1.DataSource = table_person;
+            DataSet set = new DataSet();
+            set.Tables.Add(table_color);
+            set.Tables.Add(table_person);
+
+
+            //dataGridView1.DataSource = set.Tables[0];
+            dataGridView1.DataSource = table_person;
+
+
+
+
+
+        }
+    }
+}
