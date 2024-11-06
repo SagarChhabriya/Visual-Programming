47a278a61e430f285cfac83567f9fc87fda5bd87
Sagar Chhabriya <129516589+SagarChhabriya@users.noreply.github.com>
2024-11-06 16:44:55 +0000

    Add

---
+++ FormsApp4-MultipleForm-StripMenu-/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porgramBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programAToolStripMenuItem,
+            this.porgramBToolStripMenuItem,
+            this.quitToolStripMenuItem,
+            this.calculatorToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(804, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programAToolStripMenuItem
+            // 
+            this.programAToolStripMenuItem.Name = "programAToolStripMenuItem";
+            this.programAToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.programAToolStripMenuItem.Text = "Program A";
+            this.programAToolStripMenuItem.Click += new System.EventHandler(this.programAToolStripMenuItem_Click_1);
+            // 
+            // porgramBToolStripMenuItem
+            // 
+            this.porgramBToolStripMenuItem.Name = "porgramBToolStripMenuItem";
+            this.porgramBToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.porgramBToolStripMenuItem.Text = "Porgram B";
+            this.porgramBToolStripMenuItem.Click += new System.EventHandler(this.porgramBToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // calculatorToolStripMenuItem
+            // 
+            this.calculatorToolStripMenuItem.Name = "calculatorToolStripMenuItem";
+            this.calculatorToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.calculatorToolStripMenuItem.Text = "Calculator";
+            this.calculatorToolStripMenuItem.Click += new System.EventHandler(this.calculatorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 412);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porgramBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculatorToolStripMenuItem;
+    }
+}
+
---
+++ FormsApp4-MultipleForm-StripMenu-/Form5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.WhiteSmoke;
+            WindowState = FormWindowState.Maximized;    
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Size = new Size(50, 50);
+            //set the title of label
+        /// Set the size of the label
+            label1.Size = new Size(200, 50); // Width: 200 pixels, Height: 50 pixels
+            label1.Text = "label1";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
